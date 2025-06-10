9faed564c02e307bb2708d4ac9c6468869727197
test1 <test1@gmail.com>
2025-06-10 13:08:46 +0530

    Intial terraform

---
+++ lookups.tf
@@ -0,0 +1,29 @@
+data "azurerm_platform_image" "ubuntu" {
+  location  = "Central India"
+  publisher = "Canonical"
+  offer     = "0001-com-ubuntu-server-jammy"
+  sku       = "22_04-lts"
+}
+data "azurerm_virtual_network" "default" {
+  name                = "default"
+  resource_group_name = "Terra-Test"
+}
+
+data "azurerm_subnet" "default" {
+  name                 = "default"
+  virtual_network_name = data.azurerm_virtual_network.default.name
+  resource_group_name  = data.azurerm_virtual_network.default.resource_group_name
+
+}
+
+output "subnet_id" {
+  value = data.azurerm_subnet.default.id
+}
+
+output "virtual_network_id" {
+  value = data.azurerm_virtual_network.default.id
+}
+
+output "image_id" {
+  value = data.azurerm_platform_image.ubuntu.id
+}
